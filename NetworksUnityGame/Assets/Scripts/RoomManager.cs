4d5434463ad893ea96fb54f81baab39d9ddc4c7f
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2021-01-11 00:27:15 +0100

    Creating roomManager and GameManager

---
+++ NetworksUnityGame/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+
+    public static RoomManager instance;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if(scene.buildIndex == 1)
+        {
+            PhotonNetwork.Instantiate("PhotonPrefabs/GameManager", Vector3.zero, Quaternion.identity);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
