d6a959cd8bc7c557b08a1d3b56da540c7251e1be
ponspack9 <ospoga9@gmail.com>
2021-01-16 21:25:10 +0100

    Update PlayerManagerCS.cs

--- NetworksUnityGame/Assets/Scripts/PlayerManagerCS.cs
+++ NetworksUnityGame/Assets/Scripts/PlayerManagerCS.cs
@@ -30,13 +30,37 @@ public class PlayerManagerCS : MonoBehaviour
 
     void CreateController()
     {
-        GameObject spawn = GameObject.Find("TankSpawn1");
+        var players = PhotonNetwork.CurrentRoom.PlayerCount;
 
-        Debug.Log(spawn);
+        GameObject spawn= GameObject.Find("TankSpawn1");
+        switch (players)
+        {
+            case 1:
+                spawn = GameObject.Find("TankSpawn1");
+                break;
+            case 2:
+                spawn = GameObject.Find("TankSpawn2");
+                break;
+            case 3:
+                spawn = GameObject.Find("TankSpawn3");
+                break;
+            case 4:
+                spawn = GameObject.Find("TankSpawn4");
+                break;
 
-        if (spawn)
-            spawn.gameObject.GetComponent<TankSpawner>().SpawnTank(1);
-        else if (PV.GetInstanceID() == 2)
-            GameObject.Find("TankSpawn2").gameObject.GetComponent<TankSpawner>().SpawnTank(2);
+            default:
+                break;
+        }
+        spawn.gameObject.GetComponent<TankSpawner>().SpawnTank(players);
+
+
+
+        //Debug.Log(spawn);
+
+        //    if (spawn)
+        //        spawn.gameObject.GetComponent<TankSpawner>().SpawnTank(1);
+        //    else if (PV.GetInstanceID() == 2)
+        //        GameObject.Find("TankSpawn2").gameObject.GetComponent<TankSpawner>().SpawnTank(2);
+        //
     }
 }
